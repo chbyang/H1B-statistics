705b536a9490fc4c64f85511582df050ce0c9410
Chaobin Yang <chbyangchbyang@gmail.com>
2018-10-25 19:04:54 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./src/h1b_counting_bash.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+python3 ./src/h1b_counting_bash.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
